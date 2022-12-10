84e304660a7a9f93e1bba6f64d566845bf4887d2
ralph44 <lennard.gerdes@gmx.net>
2022-12-10 12:56:13 +0100

    #build-tutorial

--- campaigns/tutorial/build-tutorial/contracts/HelloGuardian.sol
+++ campaigns/tutorial/build-tutorial/contracts/HelloGuardian.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.16;
 
 contract HelloGuardian {
 
-    /// @return "Hello Guardian"
     function hello() external pure returns (string memory) {
         return "Hello Guardian";
     }
